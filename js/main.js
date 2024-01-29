cf6e078691839b78fab5c09ac9890f1febf566fa
JocelynBD <155588614+JocelynBD@users.noreply.github.com>
2024-01-29 17:00:29 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-/// Variables para los elementos del DOM
+// Variables para los elementos del DOM
 let ip = document.getElementById("ip");
 let pais = document.getElementById("pais");
 let continente = document.getElementById("continente");
@@ -15,6 +15,49 @@ function enviarDatosABaseDeDatos(datos) {
       console.error(error);
     });
 }
+
+// Función para redirigir según el continente
+function redirigirSegunContinente() {
+  const continenteValor = continente.innerHTML;
+
+  switch (continenteValor) {
+    case 'Africa':
+      window.location.href = 'https://itp-examen26.000webhostapp.com/php-geoip-api/index.php';
+      break;
+    case 'Europa':
+      window.location.href = 'https://itp-examen.000webhostapp.com/php-geoip-api/index.php';
+      break;
+    case 'Sudamérica':
+      window.location.href = 'https://itp-scr-examen-1.000webhostapp.com/php-geoip-api/index.php';
+      break;
+    // Puedes agregar más casos según sea necesario para otros continentes
+
+    default:
+      // En caso de que no coincida con ninguno de los casos anteriores, puedes hacer algo o dejarlo vacío
+      break;
+  }
+}
+
+// Función para hacer la solicitud a la API de geolocalización y redirigir después de obtener la respuesta
+const SolicitudAPIYRedirigir = () => {
+  axios.get("https://itp-examen26.000webhostapp.com/php-geoip-api/index.php")
+    .then(function (response) {
+      console.log(response.data);
+      ip.innerHTML = response.data.ip;
+      pais.innerHTML = response.data.pais;
+      continente.innerHTML = response.data.continente;
+      zona_horaria.innerHTML = response.data.zona_horaria;
+
+      // Redirigir según el continente
+      redirigirSegunContinente();
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    .finally(function () {
+      // Si es necesario, puedes realizar alguna acción después de la solicitud de la API
+    });
+};
 
 // Función para enviar el formulario con Axios y agregar a la tabla
 function enviarFormulario() {
@@ -43,29 +86,9 @@ function enviarFormulario() {
   enviarDatosABaseDeDatos(datos);
 }
 
-// Función para hacer la solicitud a la API de geolocalización
-const SolicitudAPI = () => {
-  axios.get("https://itp-examen26.000webhostapp.com/php-geoip-api/index.php")
-    .then(function (response) {
-      console.log(response.data);
-      ip.innerHTML = response.data.ip;
-      pais.innerHTML = response.data.pais;
-      continente.innerHTML = response.data.continente;
-      zona_horaria.innerHTML = response.data.zona_horaria;
-      // No necesitas asignar valores a nombre y comida aquí, ya que estos se obtienen del formulario
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
-    .finally(function () {
-      // siempre será ejecutado
-    });
-};
-
 // Llama al evento LOAD cada vez que se refresca o se actualiza la página
-// Y llama a la función SolicitudAPI que tiene la rutina de llamada a la API desde Axios
+// Y llama a la función SolicitudAPIYRedirigir que incluye la redirección según el continente
 window.addEventListener('load', function () {
-  SolicitudAPI();
+  SolicitudAPIYRedirigir();
   // Puedes agregar más acciones después de cargar la página si es necesario
 });
-
